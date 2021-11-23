e087e332f2ba9c7e8ec644d10bc18cdff1c88969
nksharma514 <nksharma514@gmail.com>
2021-11-24 01:26:46 +0530

    Spring Boot CRUD Operation

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,61 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+//	POST METHOD
+	
+	public Product saveProduct(Product product) {
+		return repository.save(product);
+	}	
+	public List<Product> saveProducts(List<Product> products){
+		return repository.saveAll(products);
+	}
+	
+//	GET METHOD
+	
+	public List<Product> getProducts(){
+		return repository.findAll();
+	}	
+	public Product getProductById(int id) {
+		return repository.findById(id).orElse(null);
+	}
+	public Product getProductByName(String name) {
+		return repository.findByName(name);
+	}	
+	
+//	DELETE METHOD
+	
+	public String deleteProduct(int id) {
+		repository.deleteById(id);
+		return " Product Removed " +id;
+	}
+	
+//	UPDATE METHOD
+	
+	public Product updateProduct(Product product) {
+		Product existingProduct = repository.findById(product.getId()).orElse(null);
+		
+		existingProduct.setName(product.getName());
+		existingProduct.setQuantity(product.getQuantity());
+		existingProduct.setPrice(product.getPrice());
+		
+		return repository.save(existingProduct);
+	}
+	
+	
+	
+	
+
+}
